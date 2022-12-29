a2bacda3365d8322f035166eab7c994d3847fc13
Obinna <obinna.aguwa@gmail.com>
2022-12-29 19:34:41 +0100

    protocol oriented programming refactor

--- CompaniesCoreData/Companies/CompaniesController+UITableView.swift
+++ CompaniesCoreData/Companies/CompaniesController+UITableView.swift
@@ -5,4 +5,50 @@
 //  Created by Obinna Aguwa on 29/12/2022.
 //
 
-import Foundation
+import UIKit
+
+extension CompaniesController {
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let swipeActions = UISwipeActionsConfiguration(actions: [deleteActionFunction(indexPath), editActionFunction(indexPath)])
+        return swipeActions
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView()
+        view.backgroundColor = .lightBlue
+        return view
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let label = UILabel()
+        label.text = "No companies available"
+        label.textColor = .white
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        return label
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return companies.count == 0 ? 150 : 0
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath) as! CompanyCell
+        
+        let company = companies[indexPath.row]
+        cell.company = company
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return companies.count
+    }
+}
--- CompaniesCoreData/Companies/CompaniesController.swift
+++ CompaniesCoreData/Companies/CompaniesController.swift
@@ -13,9 +13,8 @@ class CompaniesController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.fetchCompanies()
         view.backgroundColor = .systemBackground
+        self.companies = CoreDataManager.shared.fetchCompanies()
         setUpNavigationStyle()
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "plus")?.withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(handleAddCompany))
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Reset", style: .plain, target: self, action: #selector(handleReset))
@@ -25,64 +24,8 @@ class CompaniesController: UITableViewController {
         tableView.register(CompanyCell.self, forCellReuseIdentifier: "cellId")
     }
     
-    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-        let swipeActions = UISwipeActionsConfiguration(actions: [deleteActionFunction(indexPath), editActionFunction(indexPath)])
-        return swipeActions
-    }
-    
-    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let view = UIView()
-        view.backgroundColor = .lightBlue
-        return view
-    }
-    
-    override func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-        let label = UILabel()
-        label.text = "No companies available"
-        label.textColor = .white
-        label.textAlignment = .center
-        label.font = UIFont.boldSystemFont(ofSize: 16)
-        return label
-    }
-    
-    override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-        return companies.count == 0 ? 150 : 0
-    }
-    
-    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 50
-    }
-    
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 60
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath) as! CompanyCell
-        
-        let company = companies[indexPath.row]
-        cell.company = company
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return companies.count
-    }
-    
     func setUpNavigationStyle() {
         navigationItem.title = "Companies"
-    }
-    
-    func fetchCompanies() {
-        let context = CoreDataManager.shared.persistentContainer.viewContext
-        let fetchRequest = NSFetchRequest<Company>(entityName: "Company")
-        
-        do {
-            companies = try context.fetch(fetchRequest)
-            tableView.reloadData()
-        } catch let fetchErr {
-            print("Failed to fetch companies: \(fetchErr)")
-        }
     }
     
     func deleteActionFunction(_ indexPath: IndexPath) -> UIContextualAction {
